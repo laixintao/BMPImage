7d275147803b7be0da13f5d8019e6536e1945eea
赖信涛 <laixintao1995@163.com>
2015-10-02 19:11:34 +0800

    refactor fileopen methods

--- main.py
+++ main.py
@@ -2,47 +2,72 @@
 __author__ = 'laixintao'
 import struct
 
+class NotBMPFileRrror(Exception):
+    pass
+
 class BMPImage(object):
-    def __init__(self,filename):
-        self.__file = open(filename)
-        self.filename=filename
-        self.signature = [struct.unpack("c",self.__file.read(1))[0],
-                          struct.unpack("c",self.__file.read(1))[0]]
-        self.size = struct.unpack("i",self.__file.read(4))[0]
-        self.reserved = struct.unpack("i",self.__file.read(4))[0]
-        self.offset = struct.unpack("i",self.__file.read(4))[0]
-        self.hdr_size = struct.unpack("i",self.__file.read(4))[0]
-        self.width = struct.unpack("i",self.__file.read(4))[0]
-        self.height = struct.unpack("i",self.__file.read(4))[0]
-        self.nr_planes = struct.unpack("h",self.__file.read(2))[0]
-        self.bit_per_pixel = struct.unpack("h",self.__file.read(2))[0]
-        self.compress_type = struct.unpack("i",self.__file.read(4))[0]
-        self.data_size = struct.unpack("i",self.__file.read(4))[0]
-        self.resol_hori = struct.unpack("i",self.__file.read(4))[0]
-        self.resol_vert = struct.unpack("i",self.__file.read(4))[0]
-        self.nr_colors = struct.unpack("i",self.__file.read(4))[0]
-        self.important_colors = struct.unpack("i",self.__file.read(4))[0]
-        self.info = []
-        for i in range(1024):
-            self.info.append(struct.unpack('B',self.__file.read(1))[0])
-        self.__file.close()
-        if self.signature[0]!="B" or self.signature[1]!="M":
-            raise IOError("Not a bmp file!")
 
-    def print_info(self):
-        d = self.__dict__
-        for key in d:
-            if key.startswith("_"):
-                pass
-            else:
-                print key,"----",d[key]
+    @staticmethod
+    def is_bmp_file(file):
+        "Check if the file is a bmpfile"
+        try:
+            f = open(file)
+            signature = [struct.unpack("c",f.read(1))[0],
+                         struct.unpack("c",f.read(1))[0]]
+        except IOError,e:
+            print e
+        finally:
+            f.close()
+        if signature[0]=="B" and signature[1]=="M":
+            return True
+        else:
+            raise NotBMPFileRrror
 
-    def print_byte(self):
-        f = open(self.filename)
-        for i in range(1000):
-            for j in range(1000):
-                print struct.unpack("b",f.read(1))[0],
-            print ""
+
+    def open(self,filename):
+        BMPImage.is_bmp_file(filename)
+        try:
+            self.__file = open(filename)
+            self.filename=filename
+            self.signature = [struct.unpack("c",self.__file.read(1))[0],
+                              struct.unpack("c",self.__file.read(1))[0]]
+
+            self.size = struct.unpack("i",self.__file.read(4))[0]
+            self.reserved = struct.unpack("i",self.__file.read(4))[0]
+            self.offset = struct.unpack("i",self.__file.read(4))[0]
+            self.hdr_size = struct.unpack("i",self.__file.read(4))[0]
+            self.width = struct.unpack("i",self.__file.read(4))[0]
+            self.height = struct.unpack("i",self.__file.read(4))[0]
+            self.nr_planes = struct.unpack("h",self.__file.read(2))[0]
+            self.bit_per_pixel = struct.unpack("h",self.__file.read(2))[0]
+            self.compress_type = struct.unpack("i",self.__file.read(4))[0]
+            self.data_size = struct.unpack("i",self.__file.read(4))[0]
+            self.resol_hori = struct.unpack("i",self.__file.read(4))[0]
+            self.resol_vert = struct.unpack("i",self.__file.read(4))[0]
+            self.nr_colors = struct.unpack("i",self.__file.read(4))[0]
+            self.important_colors = struct.unpack("i",self.__file.read(4))[0]
+            self.info = []
+            self.data = []
+            for i in range(1024):
+                self.info.append(struct.unpack('B',self.__file.read(1))[0])
+            for i in range(self.width):
+                for j in range(self.height):
+                    self.data.append(struct.unpack(
+                        "B",self.__file.read(1)
+                    )[0])
+        except IOError,e:
+            print e
+        finally:
+            self.__file.close()
+
+    def __init__(self,filename="__None"):
+        if filename != "__None":
+            self.open(filename)
+        else:
+            pass
+
+    def get_image_info(self):
+        return self.data
 
     def write_to_new_file(self,filename,threshold=108):
         f = open(filename,"wb")
@@ -76,7 +101,3 @@ class BMPImage(object):
         f.close()
         return True
 
-if __name__=="__main__":
-    image = BMPImage("test.bmp")
-    image.print_info()
-    image.write_to_new_file("new2.bmp")
